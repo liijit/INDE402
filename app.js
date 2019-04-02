c0708eb738f64f66079999ca4cecfdebeb2da42c
liijit <jit43000@gmail.com>
2019-04-02 13:03:12 +0100

    This is another test

---
+++ app.js
@@ -0,0 +1,48 @@
+const Express = require("express");
+const BodyParser = require("body-parser");
+const mongo = require("mongodb");
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectID;
+
+const dbName = "thermo-db";
+//const url = `mongodb://localhost:27017`;
+const url = "mongodb+srv://admin:12345abcde@moviequotes-cbt5l.mongodb.net/test?retryWrites=true";
+
+var app = Express();
+var port = process.env.PORT || 3000;
+
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({ extended: true }));
+
+MongoClient.connect(url, { useNewUrlParser: true }, (error, client) => {
+    if (error) throw error;
+    database = client.db(thermo-db);
+    collection = database.collection("thermocollation");
+    console.log(`Connected to ${dbName}`);
+})
+
+app.get("/", (req, res) => {
+    res.send('None shall pass');
+});
+
+app.post("/addData", (req, res) => {
+    res.status(200);
+    database.collection("thermocollation").insertOne(req.body, (err, result) => {
+        if (err) throw err;
+        console.log("Saved");
+        res.redirect("/");
+    })
+})
+
+app.get("/getData", (req, res) => {
+    res.status(200);
+    database.collection("thermocollation").find().toArray((err, result) => {
+        if (err) throw err;
+        res.send(result);
+    })
+})
+
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
