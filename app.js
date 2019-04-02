d3e59dffabfbc59c2a1bafa748889b5093871eed
liijit <jit43000@gmail.com>
2019-04-02 16:06:55 +0100

    test

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ MongoClient.connect(url, { useNewUrlParser: true }, (error, client) => {
 })
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/www/index.html");
+    res.sendFile(__dirname + "/www/");
 });
 
 app.post("/addData", (req, res) => {
